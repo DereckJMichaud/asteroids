7d1ef85eec00d7c15fc245dd1685c097f5717923
DereckJMichaud <michauddereck@gmail.com>
2024-08-19 13:59:10 -0400

    first loop ch2 2

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 
 def main():
+    pygame.init
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running == True :
+        screen.fill(pygame.Color(0,0,0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygme.quit
+                sys.exit()
+                running = False
 
 if __name__ == "__main__":
     main()
