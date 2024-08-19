8e3c16462aede45d424ed2ed7940c74e1cb00d7e
DereckJMichaud <michauddereck@gmail.com>
2024-08-19 14:32:41 -0400

    ch2 step 4

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from constants import *
 
 def main():
     pygame.init
-    class pygame.time.Clock :
-        def __init__(self, dt):
-            self.dt = 0
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,6 +16,7 @@ def main():
         screen.fill(pygame.Color(0,0,0))
         pygame.display.flip()
         
-
+        clock.tick(60)
+        dt = clock.tick()/1000
 if __name__ == "__main__":
     main()
