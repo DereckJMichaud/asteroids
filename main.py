1c335b0d6b214ce298c6eefc6d9c60d4e2648250
DereckJMichaud <michauddereck@gmail.com>
2024-08-19 14:11:05 -0400

    pause to close pygame screen

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from constants import *
 
 def main():
     pygame.init
+    class pygame.time.Clock :
+        def __init__(self, dt):
+            self.dt = 0
+
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
-    while running == True :
-        screen.fill(pygame.Color(0,0,0))
-        pygame.display.flip()
+    while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygme.quit
-                sys.exit()
-                running = False
+                return
+            
+        screen.fill(pygame.Color(0,0,0))
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
